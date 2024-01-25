58b04751c80ae5e4dc81aa730d925e9b7a7f1893
Elias Vakkuri <elias.vakkuri@brainflight.fi>
2024-01-25 14:49:55 +0200

    3 cpp exercises

---
+++ cpp/lasagna/Header.h
@@ -0,0 +1,19 @@
+//
+//  lasagna.h
+//  lasagna
+//
+//  Created by Elias Vakkuri on 25.1.2024.
+//
+
+#ifndef lasagna_h
+#define lasagna_h
+
+int ovenTime();
+
+int remainingOvenTime(int actualMinutesInOven);
+
+int preparationTime(int numberOfLayers);
+
+int elapsedTime(int numberOfLayers, int actualMinutesInOven);
+
+#endif /* lasagna_h */
